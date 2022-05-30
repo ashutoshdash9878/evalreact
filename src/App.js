a6cc854ffab80a97025e79e7292e70a08342d737
ashutoshdash9878 <101615795+ashutoshdash9878@users.noreply.github.com>
2022-05-30 15:53:22 +0530

    message

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Button } from '@chakra-ui/react';
+
+function App() {
+  return ( 
+  <div className="App">
+ <center>  <Button colorScheme='teal' size='lg'>
+    Add New Products
+  </Button></center>
+  
+  
+</div>
+  );
+};
+
+export default App;
